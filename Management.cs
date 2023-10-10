d1888a71c400675eee8bcd9d515badf45b50587c
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-10 22:52:31 +0530

    [Dhanya R Hegde] : Retrived Top 3 Records with Highest Rating

---
+++ Management.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProductReviewManagementSystem
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReview in listProductReview
+                                orderby productReview.Rating descending
+                                select productReview).Take(3);
+
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID : " + list.ProductID + " UserID : " + list.UserID + " Rating : " + list.Rating + " Review : " + list.Review + " isLike : " + list.IsLike);
+            }
+        }
+    }
+}
